04717c08d9c7722be82caa5787761c18b543fa99
jose ramirez <joseramirez@joses-MacBook-Pro.local>
2017-05-31 17:55:03 -0400

    controlling the activity indicator using a the switch with two different ways

--- SwitchesAndActivityIndicators/ViewController.swift
+++ SwitchesAndActivityIndicators/ViewController.swift
@@ -9,10 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var myIndicator: UIActivityIndicatorView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func switchDidChange(_ sender: UISwitch) {
+        /* 1.)check to see if animator is changing , thats pretty much all one can do with the activity indicator view*/
+    
+        /* if(myIndicator.isAnimating){
+            myIndicator.stopAnimating()
+        }else{
+            myIndicator.startAnimating()
+        }
+         
+        */
+    
+        /*2.) we can also read the switch being on or off
+          through the isOn or isOff property*/
+        //this should do the same thing as 1.)
+        
+        if(sender.isOn){
+            myIndicator.startAnimating()
+        }else{
+            myIndicator.stopAnimating()
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
